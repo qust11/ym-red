76c42e47b59400e343ca500224532166ec3bc128
qust <615955994@qq.com>
2025-09-22 23:52:52 +0800

    feat(components): 替换 Car 组件为 Person 组件
    移除了原有的 Car.vue 组件，更新了 App.vue 中的组件引用，
    使用 Person 组件替代原有的 Car 组件。

---
+++ 01备份/06toRefs的使用/components/Person.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="person">
+      <h1>姓名：{{name}}</h1>
+      <p>年龄:{{age}}</p>
+    <button @click="ageAdd">年龄+1</button>
+    <button @click="nameChange">姓名改为Juan</button>
+    <button @click="nameChange2">姓名改为Jua2n</button>
+</div>
+</template>
+
+<script setup name="Person" lang="ts">
+import { ref, toRefs,reactive } from 'vue'
+
+let person = reactive({'name': 'Juan','age': 23,'city': 'Mexico'})
+let {name, age, city} = toRefs(person)
+
+function ageAdd() {
+  console.log(age)
+  age.value += 1
+}
+
+function nameChange() {
+  name.value += '~'
+  // 解构的name和person.name是同一个对象 修改name.value也会修改person.name
+  console.log(name.value,person.name)
+}
+
+function nameChange2() {
+  person.name += '~'
+  // 解构的name和person.name是同一个对象 修改name.value也会修改person.name(反过来也同样生效)
+  console.log(name.value,person.name)
+}
+
+</script>
+
+<style scoped>
+.person {
+  background-color: azure;
+  padding: 10px;
+}
+
+button{
+  margin: 10px;
+}
+</style>
---
+++ src/components/Person.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="person">
+    姓：<input v-model="firstname" value="zhang" /> <br/>
+    名：<input v-model="lastname" value="san"/> <br/>
+    <h3>姓名：{{ fullname}}</h3>
+    <h3>姓名：{{ fullname}}</h3>
+    <h3>姓名：{{ fullname}}</h3>
+    <h3>姓名：{{ fullname}}</h3>
+    <h3>姓名：{{ fullname}}</h3>
+    <button @click="changeName">修改名称</button>
+  </div>
+</template>
+
+<script setup name="Person" lang="ts">
+import { get } from 'http';
+import { ref,computed, Text } from 'vue'
+
+let firstname = ref('zhang');
+let lastname = ref('san');
+
+let fullname = computed( {
+  get() {
+      // 无论上边使用多少次，只会调用一次计算属性，计算属性是有缓存的
+    console.log('1');
+    return firstname.value.slice(0,1).toUpperCase() + firstname.value.slice(1) + ' - ' + lastname.value;
+  },
+  set(val) {
+    console.log('2');
+    const [str1,str2] = val.split('-');
+    firstname.value =str1 || '';
+    lastname.value = str2 || '';
+  }
+});
+
+
+function changeName() {
+  fullname.value = 'li-si';
+}
+
+</script>
+
+<style scoped>
+.person {
+  background-color: azure;
+  padding: 10px;
+}
+
+button{
+  margin: 10px;
+}
+</style>
